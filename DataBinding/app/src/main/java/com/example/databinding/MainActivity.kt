a3dd0a66145fd28bce0964b6108fc1b1af70a146
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2025-07-30 20:08:22 +0530

    updated

---
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.student = getStudent()
+    }
+
+     private fun getStudent(): Student {
+        return Student(name = "Alex", age = 20)
+    }
+}
